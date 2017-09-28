ab1975630d5f67c6350a1871b035a17a2bb1e0aa
Vadim Vagin <vadikvs666@gmail.com>
2017-09-28 21:49:17 +0600

    add get params entity in dataaccessor

--- src/main/java/vadikvs/ife/DataAccessor.java
+++ src/main/java/vadikvs/ife/DataAccessor.java
@@ -7,6 +7,7 @@ package vadikvs.ife;
 import java.sql.Statement;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
@@ -41,6 +42,28 @@ public class DataAccessor {
                 firmList.add(firm);
             }
             return firmList ;
+        } catch (SQLException ex) {
+            Logger.getLogger(DataAccessor.class.getName()).log(Level.SEVERE, null, ex);
+        } 
+        return null;
+    }
+    
+    public ParamsEntity getParamsByFirmId(int id)  {
+        try {
+            ParamsEntity entity =null;
+            PreparedStatement st = connection.prepareStatement("select * from params_firm where firm=?");
+            st.setString(1, String.valueOf(id));
+            ResultSet result_set = st.executeQuery();       
+            while (result_set.next()) {
+                int start_row = result_set.getInt("start_row");
+                int max_row = result_set.getInt("max_row");
+                int count_col = result_set.getInt("count_col");
+                int summ_col = result_set.getInt("summ_col");
+                int artikul_col = result_set.getInt("artikul_col");
+                entity =new ParamsEntity(start_row, max_row, 
+                                                count_col, summ_col, artikul_col);
+            }
+            return entity ;
         } catch (SQLException ex) {
             Logger.getLogger(DataAccessor.class.getName()).log(Level.SEVERE, null, ex);
         } 
