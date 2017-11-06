68c91f5951d733d9e805d1c78063d089b2826724
Vadim Vagin <vadikvs666@gmail.com>
2017-11-06 20:25:48 +0600

    search firm

--- src/main/java/vadikvs/ife/MainController.java
+++ src/main/java/vadikvs/ife/MainController.java
@@ -7,8 +7,11 @@ import java.util.List;
 import java.util.ResourceBundle;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javafx.beans.InvalidationListener;
+import javafx.beans.Observable;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
@@ -16,6 +19,8 @@ import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.stage.Stage;
 
 public class MainController implements Initializable {
@@ -51,6 +56,8 @@ public class MainController implements Initializable {
     private TextField numberRowTextEdit;
     @FXML
     private TextField maxRowTextFiled;
+    @FXML
+    private TextField searchText;
     private Stage stage;
 
     public void setStage(Stage stage) {
@@ -91,30 +98,54 @@ public class MainController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
+
+        try {
+            getAllFrimFromDB();
+            onFirmSelect();
+        } catch (Exception ex) {
+            Settings settings = new Settings();
+            SettingsFormGenerator form = new SettingsFormGenerator(settings);
+            form.show();
+        }
+
+    }
+
+    private void getAllFrimFromDB() {
         Settings settings = new Settings();
         String server = settings.getValue("server");
         String user = settings.getValue("user");
         String db = settings.getValue("database");
         String password = settings.getValue("password");
-        try {
-            String conString = "jdbc:mysql://";
-            conString += server;
-            conString += "/";
-            conString += db;
-            this.DA = new DataAccessor("com.mysql.jdbc.Driver", conString, user, password);
-            List<FirmEntity> list = DA.getFirmList();
-            idColumn.setCellValueFactory(new PropertyValueFactory<>("id"));
-            nameColumn.setCellValueFactory(new PropertyValueFactory<>("name"));
-            firmData = FXCollections.observableArrayList(list);
-            firmTableView.setItems(firmData);
-            firmTableView.getSelectionModel().select(0);
-            onFirmSelect();
-        } catch (Exception ex) {
-            settings = new Settings();
-            SettingsFormGenerator form = new SettingsFormGenerator(settings);
-            form.show();
-        }
+        String conString = "jdbc:mysql://";
+        conString += server;
+        conString += "/";
+        conString += db;
+        this.DA = new DataAccessor("com.mysql.jdbc.Driver", conString, user, password);
+        List<FirmEntity> list = DA.getFirmList();
+        idColumn.setCellValueFactory(new PropertyValueFactory<>("id"));
+        nameColumn.setCellValueFactory(new PropertyValueFactory<>("name"));
+        firmData = FXCollections.observableArrayList(list);
+        firmTableView.setItems(firmData);
+        firmTableView.getSelectionModel().select(0);
+    }
 
+    private void getFirmFromDBLikeName(String name) {
+        Settings settings = new Settings();
+        String server = settings.getValue("server");
+        String user = settings.getValue("user");
+        String db = settings.getValue("database");
+        String password = settings.getValue("password");
+        String conString = "jdbc:mysql://";
+        conString += server;
+        conString += "/";
+        conString += db;
+        this.DA = new DataAccessor("com.mysql.jdbc.Driver", conString, user, password);
+        List<FirmEntity> list = DA.getFirmListLikeName(name);
+        idColumn.setCellValueFactory(new PropertyValueFactory<>("id"));
+        nameColumn.setCellValueFactory(new PropertyValueFactory<>("name"));
+        firmData = FXCollections.observableArrayList(list);
+        firmTableView.setItems(firmData);
+        firmTableView.getSelectionModel().select(0);
     }
 
     public void onFirmSelect() {
@@ -151,5 +182,28 @@ public class MainController implements Initializable {
 
     private FirmEntity getCurrentFirm() {
         return firmTableView.getSelectionModel().selectedItemProperty().getValue();
+    }
+
+    @FXML
+    public void onSearch(KeyEvent event) {
+        if (event.getCode() == KeyCode.ENTER) {
+            getFirmFromDBLikeName(searchText.textProperty().get().toLowerCase());
+        }
+    }
+
+    private void initFilter() {
+        searchText.textProperty().addListener(new InvalidationListener() {
+            @Override
+            public void invalidated(Observable observable) {
+                if (searchText.textProperty().get().isEmpty()) {
+                    getAllFrimFromDB();
+                    return;
+                } else {
+                    getFirmFromDBLikeName(searchText.textProperty().get().toLowerCase());
+                }
+
+            }
+        });
+
     }
 }
